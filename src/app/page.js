cc4b74a4e20f3631c152f9e98f51b36342419292
lukerabbitte <luke.rabbitte@gmail.com>
2024-11-07 04:53:10 +0000

    Messing around with text parallax stuff but fun

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const handleScroll = () => {
-      const letters = document.querySelectorAll(".parallax-letter");
+    const handleLetters = (letters) => {
       letters.forEach((letter, index) => {
         const rateOfChange = Math.random();
         if (window.scrollY === 0) {
@@ -20,6 +19,23 @@ export default function Home() {
       });
     };
 
+    const handleFasterMovingText = (fasterMovingText, maxOffset) => {
+      fasterMovingText.forEach((text) => {
+        const offset = Math.min(window.scrollY * 1.05, maxOffset);
+        text.style.transform = `translateY(-${offset}px)`;
+      });
+    };
+
+    const handleScroll = () => {
+      const letters = document.querySelectorAll(".parallax-letter");
+      const fasterMovingText = document.querySelectorAll(".faster-moving-text");
+      const klimtBg = document.querySelector(".bg-klimt-pattern");
+      const maxOffset = klimtBg.offsetHeight;
+
+      handleLetters(letters);
+      handleFasterMovingText(fasterMovingText, maxOffset);
+    };
+
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -29,7 +45,7 @@ export default function Home() {
   return (
     <main className="flex flex-col h-screen">
       <div className="flex flex-col gap-4">
-        <h1 className="text-8xl font-bold p-2 h-96 ml-auto break-words place-content-end -tracking-tighter">
+        <h1 className="text-8xl font-bold p-2 h-3/4 lg:h-96 ml-auto break-words place-content-end -tracking-tighter">
           {"Caroline Kelly".split("").map((char, index) => (
             <span
               key={index}
@@ -39,12 +55,35 @@ export default function Home() {
             </span>
           ))}
         </h1>
-        <div className="bg-klimt-pattern w-full lg:w-3/4 h-96 p-4 items-center place-self-start place-content-center">
-          <h1 className="p-4 text-4xl text-yellow-50 rounded-md border-2 border-yellow-50">
-            Dublin-based journalist
-          </h1>
+        <div className="relative w-full h-96 p-4 items-center place-self-start place-content-center top-0 z-0">
+          <div className="absolute inset-0 bg-klimt-pattern filter blur-lg"></div>
         </div>
-        <div></div>
+        <div className="faster-moving-text z-30">
+          <div className="flex flex-row p-4 mt-6 items-center rounded-md w-1/2 h-36 backdrop-blur-xl border-2 border-muted-foreground">
+            <p className="text-4xl text-yellow-50">Check out my articles <a href="/posts">here</a>?</p>
+          </div>
+        </div>
+        <div className="faster-moving-text z-30">
+          <div className="flex flex-row p-4 ml-auto items-center rounded-md w-1/2 h-36 backdrop-blur-xl border-2 border-muted-foreground">
+            <p className="text-4xl text-yellow-50">Sure thing babe ❤️</p>
+          </div>
+        </div>
+        <h1 className="text-4xl text-yellow-50">
+          Dublin-based journalist gifted in the art of telling things how they
+          are
+        </h1>
+        <h1 className="text-4xl text-yellow-50">
+          Dublin-based journalist gifted in the art of telling things how they
+          are
+        </h1>
+        <h1 className="text-4xl text-yellow-50">
+          Dublin-based journalist gifted in the art of telling things how they
+          are
+        </h1>
+        <h1 className="text-4xl text-yellow-50">
+          Dublin-based journalist gifted in the art of telling things how they
+          are
+        </h1>
       </div>
     </main>
   );
