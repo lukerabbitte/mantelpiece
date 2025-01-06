512a2d274dc64b8d94578010f243cc409093519e
lukerabbitte <luke.rabbitte@gmail.com>
2025-01-06 05:01:07 +0000

    Adds current page indicator on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@
 
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaHome, FaList } from "react-icons/fa";
 import ThemeToggle from "@/components/ThemeToggle";
 import { Button } from "@/components/ui/button";
 
 const Navbar = () => {
     const [buttonVariant, setButtonVariant] = useState("default");
+    const pathname = usePathname();
 
     useEffect(() => {
         const handleScroll = () => {
@@ -24,10 +26,14 @@ const Navbar = () => {
         };
     }, []);
 
+    const getButtonVariant = (path) => {
+        return pathname === path ? "default" : buttonVariant;
+    };
+
     return (
         <div className="sticky top-0 z-50 flex flex-row justify-between items-center p-4">
             <div className="flex flex-row gap-4">
-                <Button asChild variant={buttonVariant} size="evenPad" title="Home">
+                <Button asChild variant={getButtonVariant("/")} size="evenPad" title="Home">
                     <Link href="/">
                         <div className="bg-primary-foreground rounded-md flex justify-center items-center">
                             <FaHome className="text-foreground p-[0.4em] w-6 h-6" />
@@ -35,7 +41,12 @@ const Navbar = () => {
                     </Link>
                 </Button>
 
-                <Button asChild variant={buttonVariant} size="evenPad" title="Articles">
+                <Button
+                    asChild
+                    variant={getButtonVariant("/posts")}
+                    size="evenPad"
+                    title="Articles"
+                >
                     <Link href="/posts">
                         <div className="bg-primary-foreground rounded-md flex justify-center items-center">
                             <FaList className="text-foreground p-[0.4em] w-6 h-6" />
